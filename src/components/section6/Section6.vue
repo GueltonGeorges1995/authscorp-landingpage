9ac32f13692de6cf7beb57d978758708cdbb10e7
Ryan <ryanfrynelson@hotmail.fr>
2020-02-18 16:22:23 +0100

    Section 6

---
+++ src/components/section6/Section6.vue
@@ -0,0 +1,288 @@
+<template>
+  <v-app>
+    <section>
+      <v-row class="mb-10">
+        <v-col offset="2" md="4" lg="4">
+          <div class="pill">
+            <h3 class="text-center light-blue--text">AML</h3>
+          </div>
+          <h2>Payment Systems</h2>
+          <p class="subtitle">Integrate payment details of your users to authscorp AML system to get a more accurate overview of your user risks.</p>
+          <v-card class="card-box" max-width="420" outlined>
+            <v-row>
+              <v-col md="10" lg="10 pa-0">
+                <div class="ma-7">
+                  <h3 class="card-title">Visa/Mastercard/...</h3>
+                  <p class="card-sub">Connect fraudes reporting from your payment service provider.</p>
+                </div>
+              </v-col>
+              <v-col class="pa-0 d-flex flex-column justify-end">
+                <div class="pb-4">
+                  <button>
+
+                  
+                  <svg width="50px" height="50px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 63.1 (92452) - https://sketch.com -->
+                      <title>download</title>
+                      <desc>Created with Sketch.</desc>
+                      <defs>
+                          <rect id="path-1" x="0" y="0" width="339.526068" height="185.021668" rx="5"></rect>
+                          <filter x="-12.4%" y="-16.2%" width="124.7%" height="145.4%" filterUnits="objectBoundingBox" id="filter-2">
+                              <feOffset dx="0" dy="12" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                              <feGaussianBlur stdDeviation="12" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                              <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.1 0" type="matrix" in="shadowBlurOuter1" result="shadowMatrixOuter1"></feColorMatrix>
+                              <feOffset dx="-5" dy="0" in="SourceAlpha" result="shadowOffsetOuter2"></feOffset>
+                              <feColorMatrix values="0 0 0 0 0.117647059   0 0 0 0 0.839215686   0 0 0 0 0.376470588  0 0 0 1 0" type="matrix" in="shadowOffsetOuter2" result="shadowMatrixOuter2"></feColorMatrix>
+                              <feMerge>
+                                  <feMergeNode in="shadowMatrixOuter1"></feMergeNode>
+                                  <feMergeNode in="shadowMatrixOuter2"></feMergeNode>
+                              </feMerge>
+                          </filter>
+                      </defs>
+                      <g id="Landing-Page" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="KYC-/-AML" transform="translate(-559.000000, -4768.000000)">
+                              <g id="LEARN" transform="translate(2.000000, 4108.000000)">
+                                  <polygon id="bg" fill="#F5F9FF" points="0 0 1401.91409 0 1401.91409 1350 0 1350"></polygon>
+                                  <g id="cards" transform="translate(277.462163, 534.588299)">
+                                      <g id="whitepaper">
+                                          <g id="bg">
+                                              <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+                                              <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                                          </g>
+                                          <g id="download" transform="translate(280.382817, 125.785482)">
+                                              <ellipse id="bg" fill="#1ED660" cx="20.4445804" cy="20.4767064" rx="20.4445804" ry="20.4767064"></ellipse>
+                                              <g id="data-download" transform="translate(11.682617, 11.700975)" stroke="#FFFFFF" stroke-width="2">
+                                                  <line x1="8.76196304" y1="0" x2="8.76196304" y2="12.954651" id="Path"></line>
+                                                  <polyline id="Path" stroke-linecap="square" points="13.3515627 8.35783934 8.76196304 12.954651 4.17236335 8.35783934"></polyline>
+                                                  <path d="M0,12.118867 L0,15.4620028 C0,16.3851842 0.747212281,17.1335707 1.66894534,17.1335707 L15.8549807,17.1335707 C16.7767138,17.1335707 17.5239261,16.3851842 17.5239261,15.4620028 L17.5239261,12.118867" id="Path" stroke-linecap="square"></path>
+                                              </g>
+                                          </g>
+                                      </g>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  </button>
+                </div>
+              </v-col>
+
+            </v-row>
+            
+          </v-card>
+           <v-card class="card-plain" max-width="420" outlined>
+            <v-row>
+              <v-col md="10" lg="10 pa-0">
+                <div class="ma-7">
+                  <h3 class="card-title">Cryptos</h3>
+                  <p class="card-sub">Integrate chainalysis to classify all the crypto transactions.</p>
+                </div>
+              </v-col>
+              <v-col class="pa-0 d-flex flex-column justify-end">
+                <div class="pb-4">
+                  <button>
+                  <svg width="50px" height="50px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 63.1 (92452) - https://sketch.com -->
+                      <title>download</title>
+                      <desc>Created with Sketch.</desc>
+                      <defs>
+                          <rect id="path-1" x="0" y="0" width="339.526068" height="185.021668" rx="5"></rect>
+                          <filter x="-12.4%" y="-16.2%" width="124.7%" height="145.4%" filterUnits="objectBoundingBox" id="filter-2">
+                              <feOffset dx="0" dy="12" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                              <feGaussianBlur stdDeviation="12" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                              <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.1 0" type="matrix" in="shadowBlurOuter1" result="shadowMatrixOuter1"></feColorMatrix>
+                              <feOffset dx="-5" dy="0" in="SourceAlpha" result="shadowOffsetOuter2"></feOffset>
+                              <feColorMatrix values="0 0 0 0 0.117647059   0 0 0 0 0.839215686   0 0 0 0 0.376470588  0 0 0 1 0" type="matrix" in="shadowOffsetOuter2" result="shadowMatrixOuter2"></feColorMatrix>
+                              <feMerge>
+                                  <feMergeNode in="shadowMatrixOuter1"></feMergeNode>
+                                  <feMergeNode in="shadowMatrixOuter2"></feMergeNode>
+                              </feMerge>
+                          </filter>
+                      </defs>
+                      <g id="Landing-Page" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="KYC-/-AML" transform="translate(-559.000000, -4768.000000)">
+                              <g id="LEARN" transform="translate(2.000000, 4108.000000)">
+                                  <polygon id="bg" fill="#F5F9FF" points="0 0 1401.91409 0 1401.91409 1350 0 1350"></polygon>
+                                  <g id="cards" transform="translate(277.462163, 534.588299)">
+                                      <g id="whitepaper">
+                                          <g id="bg">
+                                              <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+                                              <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                                          </g>
+                                          <g id="download" transform="translate(280.382817, 125.785482)">
+                                              <ellipse id="bg" fill="#1ED660" cx="20.4445804" cy="20.4767064" rx="20.4445804" ry="20.4767064"></ellipse>
+                                              <g id="data-download" transform="translate(11.682617, 11.700975)" stroke="#FFFFFF" stroke-width="2">
+                                                  <line x1="8.76196304" y1="0" x2="8.76196304" y2="12.954651" id="Path"></line>
+                                                  <polyline id="Path" stroke-linecap="square" points="13.3515627 8.35783934 8.76196304 12.954651 4.17236335 8.35783934"></polyline>
+                                                  <path d="M0,12.118867 L0,15.4620028 C0,16.3851842 0.747212281,17.1335707 1.66894534,17.1335707 L15.8549807,17.1335707 C16.7767138,17.1335707 17.5239261,16.3851842 17.5239261,15.4620028 L17.5239261,12.118867" id="Path" stroke-linecap="square"></path>
+                                              </g>
+                                          </g>
+                                      </g>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  </button>
+                </div>
+              </v-col>
+
+            </v-row>
+            
+          </v-card>
+           <v-card class="card-plain" max-width="420" outlined>
+            <v-row>
+              <v-col md="10" lg="10 pa-0">
+                <div class="ma-7">
+                  <h3 class="card-title">Custom</h3>
+                  <p class="card-sub">Integrate events from your custom payment gateway</p>
+                </div>
+              </v-col>
+              <v-col class="pa-0 d-flex flex-column justify-end">
+                <div class="pb-4">
+                  <button>
+                  <svg  width="50px" height="50px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 63.1 (92452) - https://sketch.com -->
+                      <title>download</title>
+                      <desc>Created with Sketch.</desc>
+                      <defs>
+                          <rect id="path-1" x="0" y="0" width="339.526068" height="185.021668" rx="5"></rect>
+                          <filter x="-12.4%" y="-16.2%" width="124.7%" height="145.4%" filterUnits="objectBoundingBox" id="filter-2">
+                              <feOffset dx="0" dy="12" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+                              <feGaussianBlur stdDeviation="12" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+                              <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.1 0" type="matrix" in="shadowBlurOuter1" result="shadowMatrixOuter1"></feColorMatrix>
+                              <feOffset dx="-5" dy="0" in="SourceAlpha" result="shadowOffsetOuter2"></feOffset>
+                              <feColorMatrix values="0 0 0 0 0.117647059   0 0 0 0 0.839215686   0 0 0 0 0.376470588  0 0 0 1 0" type="matrix" in="shadowOffsetOuter2" result="shadowMatrixOuter2"></feColorMatrix>
+                              <feMerge>
+                                  <feMergeNode in="shadowMatrixOuter1"></feMergeNode>
+                                  <feMergeNode in="shadowMatrixOuter2"></feMergeNode>
+                              </feMerge>
+                          </filter>
+                      </defs>
+                      <g id="Landing-Page" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="KYC-/-AML" transform="translate(-559.000000, -4768.000000)">
+                              <g id="LEARN" transform="translate(2.000000, 4108.000000)">
+                                  <polygon id="bg" fill="#F5F9FF" points="0 0 1401.91409 0 1401.91409 1350 0 1350"></polygon>
+                                  <g id="cards" transform="translate(277.462163, 534.588299)">
+                                      <g id="whitepaper">
+                                          <g id="bg">
+                                              <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+                                              <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                                          </g>
+                                          <g id="download" transform="translate(280.382817, 125.785482)">
+                                              <ellipse id="bg" fill="#1ED660" cx="20.4445804" cy="20.4767064" rx="20.4445804" ry="20.4767064"></ellipse>
+                                              <g id="data-download" transform="translate(11.682617, 11.700975)" stroke="#FFFFFF" stroke-width="2">
+                                                  <line x1="8.76196304" y1="0" x2="8.76196304" y2="12.954651" id="Path"></line>
+                                                  <polyline id="Path" stroke-linecap="square" points="13.3515627 8.35783934 8.76196304 12.954651 4.17236335 8.35783934"></polyline>
+                                                  <path d="M0,12.118867 L0,15.4620028 C0,16.3851842 0.747212281,17.1335707 1.66894534,17.1335707 L15.8549807,17.1335707 C16.7767138,17.1335707 17.5239261,16.3851842 17.5239261,15.4620028 L17.5239261,12.118867" id="Path" stroke-linecap="square"></path>
+                                              </g>
+                                          </g>
+                                      </g>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  </button>
+                </div>
+              </v-col>
+
+            </v-row>
+            
+          </v-card>
+        </v-col>
+        <v-col class="d-flex align-center">
+          <img src="../../assets/images/computer.png" alt="">           
+        </v-col>
+      </v-row>
+    </section>      
+  </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Section6'
+
+}
+</script>
+
+<style scoped>
+  section{
+    font-family: 'Lato', sans-serif;
+    background-color: #F5F9FF;
+    padding-top: 15vh;    
+  }
+  img{
+    width: 100%;
+  }
+  h2{
+    
+    font-size: 50px;
+    color: #313236;
+  }
+  p{
+    font-size: 19px;
+    color: #263035;
+    font-weight: bold;
+    opacity: 0.8;
+    letter-spacing: 0;
+    line-height: 180%;
+  }
+  .subtitle{
+    width: 90%;
+    margin-bottom: 5vh;
+  }
+  .pill{
+    width: 25%;
+    background-color: white;
+    border-radius: 20px;
+    padding: 10px;
+    margin-bottom: 5vh;
+  }
+  .card-box{
+    background: #FFFFFF;
+    box-shadow: -5px 0 0 0 #1ED660, 0 12px 24px 0 rgba(0,0,0,0.10);
+    border-radius: 5px;
+    background: #1ED660;
+    border: 2px solid #FFFFFF;
+    border: 2px solid #FFFFFF;
+    border: 2px solid #FFFFFF;  
+    margin-bottom: 4vh; 
+  }
+  .card-plain{
+    background: #FFFFFF;   
+    border-radius: 5px;
+    background: #1ED660;
+    border: 2px solid #FFFFFF;
+    border: 2px solid #FFFFFF;
+    border: 2px solid #FFFFFF;  
+    margin-bottom: 4vh; 
+
+  }
+  .card-title{
+    font-size: 32px;
+    color: #264EEE;
+    line-height: 180%;
+    padding-top: 15px;
+  }
+  .card-sub{
+    font-size: 20px;
+    color: #7C7D8D;
+    letter-spacing: 0;
+    line-height: 180%;
+    padding-bottom: 15px;
+  }
+  button{
+    outline: none;
+  }
+  .play{
+    position: absolute;
+    top: 50%;
+    bottom: 50%;
+    left: 50%;
+    right: 50%;
+  }
+  .relative{
+    position: relative;
+  }
+</style>
