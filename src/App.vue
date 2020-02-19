f90ab88e9cfeb6d3f7e471fdd4237f45f435d0bd
Georges <georges.guelton@gmail.com>
2020-02-19 16:52:38 +0100

    app rempli

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <v-app>
     <Home />
     <News />
-    <!-- <Section3 /> -->
-    <!-- <Roadmap /> -->
-    <!-- <Pricing /> -->
+    <Section3 />
+    <Roadmap />
+    <Pricing />
      <Section6 />  
     <Footer /> 
 
@@ -15,9 +15,9 @@
 <script>
 import Home from "./components/section1/Home";
 import News from "./components/section2/News";
-// import Section3 from "./components/section3/Section3";
-// import Roadmap from "./components/section4/Roadmap";
-// import Pricing from "./components/section5/Pricing";
+import Section3 from "./components/section3/Section3";
+import Roadmap from "./components/section4/Roadmap";
+import Pricing from "./components/section5/Pricing";
 import Section6 from "./components/section6/Section6";
 import Footer from "./components/footer/Footer";
 
@@ -25,13 +25,13 @@ import Footer from "./components/footer/Footer";
 export default {
   name: "App",
   components: {
-    // Section3,
+    Section3,
     Section6,
     Home,
     News,
     Footer,
-    // Roadmap,
-    // Pricing,
+    Roadmap,
+    Pricing,
     // Test
   }
 };
