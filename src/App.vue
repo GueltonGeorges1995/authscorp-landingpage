4c38eed83e7d843b9047786094338de0b2d475a7
Georges <georges.guelton@gmail.com>
2020-02-19 16:15:19 +0100

    cleaning doc

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Home />
-     <News />
+    <News />
     <Section3 />
     <Roadmap />
     <Pricing />
@@ -16,12 +16,11 @@
 import Home from "./components/section1/Home";
 import News from "./components/section2/News";
 import Section3 from "./components/section3/Section3";
-import Roadmap from "./components/Roadmap";
-import Pricing from "./components/Pricing";
+import Roadmap from "./components/section4/Roadmap";
+import Pricing from "./components/section5/Pricing";
 import Section6 from "./components/section6/Section6";
 import Footer from "./components/footer/Footer";
 
-// import Test from './components/Test';
 
 export default {
   name: "App",
