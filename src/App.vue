7ad685a797d69e960f934f38187c9ba87d588562
Georges <georges.guelton@gmail.com>
2020-02-21 13:49:04 +0100

    Sign in Sign Up vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-    <!-- <LandingPage /> -->
-    <!-- <WelcomePage /> -->
-    <SignIn />
+  <div>
+    <SignUp />
+  </div>
 </template>
 
 <script>
-// import LandingPage from './components/LandingPage';
-// import WelcomePage from './components/WelcomePage';
-import SignIn from './components/SignIn';
+// import LandingPage from "./components/LandingPage";
+// import WelcomePage from "./components/WelcomePage";
+// import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 export default {
   name: "App",
   components: {
     // LandingPage,
-    // WelcomePage,
-    SignIn,
+    // // WelcomePage,
+    // SignIn,
+    SignUp
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+// import VueRouter from 'vue-router'
+
+// Vue.use(VueRouter);
+
+// const router = new VueRouter({
+//     mode: 'history',
+//     routes: [{
+//         path: '/',
+//         component: require('./components/LandingPage.vue')
+//     }]
+// })
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+ 
   render: h => h(App)
 }).$mount('#app')
