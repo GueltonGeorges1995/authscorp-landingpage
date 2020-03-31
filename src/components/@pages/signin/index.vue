7ed8192f52057beed9e6bda508a34955969edcf5
sacha vandamme <sacha@skyhark.be>
2020-03-31 04:55:28 +0200

    signin using openid

--- src/components/@pages/signin/index.vue
+++ src/components/@pages/signin/index.vue
@@ -4,8 +4,8 @@
       <v-col cols="12" sm="12" md="12" class="flex-mobile">
         <div class="bg-col">
           <h2 class="mb-4">Sign In</h2>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field v-model="email" outlined :rules="emailRules" label="E-mail" required></v-text-field>
+          <form ref="form" action="http://10.3.146.65:3030/webapi/auth/connect" authscorp @apiPrepare="err = null" @apiError="onError">
+            <v-text-field v-model="email" outlined :rules="emailRules" label="E-mail" name="username" />
 
             <v-text-field
               outlined
@@ -14,82 +14,91 @@
               :rules="passwordRules"
               :type="show1 ? 'text' : 'password'"
               label="Password"
+              name="password"
               @click:append="show1 = !show1"
             />
 
-            <v-btn :disabled="!valid" color="primary" class="mr-4" href="/admin">Sign In</v-btn>
-          </v-form>
+            <v-alert type="error" outlined class="text-left" :value="err !== null">{{err}}</v-alert>
+            <v-btn :disabled="!valid" color="primary" class="mr-4" type="submit">Sign In</v-btn>
+          </form>
           <br />
-          
+
           <router-link to="/signup">I Don't Have An Account yet</router-link>
           <br />
           <br />
           <router-link to="/forgotpassword">I Have Forgot My Password</router-link>
-          
         </div>
       </v-col>
     </v-container>
   </div>
 </template>
 
-
-<style scoped>
-@media screen and (max-width: 960px) {
-  .flex-mobile {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 3rem;
-  }
-  .bg-main-section {
-    padding: 1rem !important;
-  }
-  .bg-col {
-    padding: 1rem !important;
-  }
-}
-
-h2 {
-  color: #264eee;
-  font-family: Helvetica;
-}
-
-a {
-  text-decoration: none;
-}
-.bg-main-section {
-  padding: 10rem;
-  background-color: #f5f9ff;
-  text-align: center;
-}
-.bg-col {
-  background-color: #ffffff;
-  padding: 10rem;
-  border-radius: 10px;
-}
-</style>
-
-
 <script>
-export default {
-  data: () => ({
-    show1: false,
-    password: "",
-    valid: true,
-    email: "",
-    emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-    ],
-    passwordRules: [v => !!v || "Password is required"],
-  }),
+  import AuthscorpLib from '../../../authscorp-lib'
 
-  methods: {
-    validate() {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true;
+  AuthscorpLib.init()
+
+  export default {
+    data: () => ({
+      show1: false,
+      password: "",
+      valid: true,
+      err: null,
+      email: "",
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ],
+      passwordRules: [v => !!v || "Password is required"],
+    }),
+
+    methods: {
+      validate() {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true;
+        }
+      },
+      onError(e) {
+        e.preventDefault()
+        this.err = e.error
       }
     }
-  }
-};
+  };
+
 </script>
+
+<style scoped>
+  @media screen and (max-width: 960px) {
+    .flex-mobile {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 3rem;
+    }
+    .bg-main-section {
+      padding: 1rem !important;
+    }
+    .bg-col {
+      padding: 1rem !important;
+    }
+  }
+
+  h2 {
+    color: #264eee;
+    font-family: Helvetica;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  .bg-main-section {
+    padding: 10rem;
+    background-color: #f5f9ff;
+    text-align: center;
+  }
+  .bg-col {
+    background-color: #ffffff;
+    padding: 10rem;
+    border-radius: 10px;
+  }
+</style>
